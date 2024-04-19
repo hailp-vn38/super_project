bf13d6151feaff664d423aadc5aad677b26d0c74
LamPhucHai <lamphuchai.dev@gmail.com>
2024-04-20 00:24:23 +0700

    up exts

--- super_exts/plugins/anime_hay/src/chapter.js
+++ super_exts/plugins/anime_hay/src/chapter.js
@@ -7,21 +7,59 @@ async function chapter(url) {
   const regex = /_video_player\.innerHTML\s*=\s*'([^']+)'/g;
   const matches = [...res.matchAll(regex)].map((match) => match[1]);
 
+  function getIdFromUrlWithRegex(url) {
+    // Biểu thức chính quy để khớp với phần ID trong URL
+    const idRegex = /id=(?<id>[a-z0-9]+)/i;
+
+    // Tìm kiếm khớp với biểu thức chính quy trong URL
+    const match = idRegex.exec(url);
+
+    // Nếu tìm thấy khớp, trả về giá trị ID
+    if (match) {
+      return match.groups.id;
+    } else {
+      // Không tìm thấy ID, trả về null
+      return null;
+    }
+  }
+
   for (var i = 0; i < matches.length; i++) {
     var item = matches[i];
+
     var el = await Extension.getAttributeText(item, "iframe", "src");
+    console.log(getIdFromUrlWithRegex(el));
+
     if (el) {
-      result.push({
-        server_name: `SERVER-${i + 1}`,
-        data: el,
-        type: "iframe",
-      });
+      var id = getIdFromUrlWithRegex(el);
+      if (el.includes("pho.php") && id) {
+        var id = getIdFromUrlWithRegex(el);
+        result = [
+          {
+            server_name: `PHO`,
+            data: `https://pt.rapovideo.xyz/playlist/${id}/master.m3u8`,
+            type: "file",
+          },
+          ...result,
+        ];
+      } else {
+        result.push({
+          server_name: `SERVER-${i + 1}`,
+          data: el,
+          type: "iframe",
+        });
+      }
     }
   }
 
   return Response.success(result);
 }
 
-// runFn(() =>
-//   chapter("https://animehay.blog/xem-phim/tran-hon-nhai-phan-3-tap-9-58893.html")
-// );
+runFn(() =>
+  chapter(
+    "https://animehay.blog/xem-phim/tran-hon-nhai-phan-3-tap-9-58893.html"
+  )
+);
+
+// File m3u8 https://pt.rapovideo.xyz/playlist/id/master.m3u8
+//
+// https://rapovideo.xyz/playlist/6615eb0a13e90edd176c6330/master.m3u8
